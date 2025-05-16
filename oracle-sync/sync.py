518577775a675430527ae77c32b374153fb3b9fe
Greg <gregso@users.noreply.github.com>
2025-05-16 17:40:25 +0200

    dockerfile mods

--- oracle-sync/sync.py
+++ oracle-sync/sync.py
@@ -32,7 +32,7 @@ ORACLE_DSN = os.getenv('ORACLE_DSN') or f"{ORACLE_HOST}:{ORACLE_PORT}/{ORACLE_SE
 
 CLICKHOUSE_HOST = os.getenv('CLICKHOUSE_HOST', 'clickhouse')
 CLICKHOUSE_USER = os.getenv('CLICKHOUSE_USER', 'default')
-CLICKHOUSE_PASSWORD = os.getenv('CLICKHOUSE_PASSWORD', '')
+CLICKHOUSE_PASSWORD = os.getenv('CLICKHOUSE_PASSWORD', 'default')
 CLICKHOUSE_DB = os.getenv('CLICKHOUSE_DB', 'support_analytics')
 
 # Set Oracle environment variables
